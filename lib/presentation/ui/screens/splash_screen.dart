701adc92ffd8e63d5538cdd8430e1214fb7a2c9a
Saikat Barua <saikatbarua2196@gmail.com>
2026-03-30 08:49:15 +0600

    create layout

---
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:crafty_bay/presentation/ui/utilities/asset_paths.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SvgPicture.asset(AssetPaths.logoSvg),
+          ],
+        
+            ),
+      ),);
+  }
+}
---
+++ lib/presentation/ui/utilities/asset_paths.dart
@@ -0,0 +1,6 @@
+class AssetPaths{
+  static const String _basePath = "assets";
+  static const String _imagePath = "$_basePath/images";
+
+  static const String logoSvg = "$_imagePath/logo.svg";
+}
